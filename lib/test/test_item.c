a9631f969288fbef8b8be3c7bbdfde95de24d923
caaallum <callum@spei.rs>
2022-10-02 15:33:38 +0100

    Update check output to normal

--- lib/test/test_item.c
+++ lib/test/test_item.c
@@ -134,7 +134,7 @@ int main(void) {
     s = item_suite();
     sr = srunner_create(s);
 
-    srunner_run_all(sr, CK_VERBOSE);
+    srunner_run_all(sr, CK_NORMAL);
     number_failed = srunner_ntests_failed(sr);
 
     srunner_free(sr);
