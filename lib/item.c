26c1e27b0a12d7cfd6ab29bdb2c579627320b61e
caaallum <callum_speirs@protonmail.com>
2022-12-21 18:47:39 +0000

    Update comments

--- lib/item.c
+++ lib/item.c
@@ -49,7 +49,7 @@ item_list_delete(item_list_t *list, int index) {
         return;
     }
 
-    todo_item_free(list->items[index]);
+    item_free(list->items[index]);
     list->items[index] = NULL;
 
     for (int i = index; i < list->total - 1; i++) {
@@ -76,20 +76,20 @@ item_list_delete_id(item_list_t *list, int id) {
 
 void
 item_list_free(item_list_t *list) {
-    todo_free(list->items);
-    todo_free(list);
+    free(list->items);
+    free(list);
 }
 
 void
 item_list_free_all(item_list_t *list) {
     for (int i = 0; i < list->total; i++) {
-        todo_item_free(list->items[i]);
+        item_free(list->items[i]);
     }
     item_list_free(list);
 }
 
 item_t *
-todo_item_new(void) {
+item_new(void) {
     item_t *item = malloc(sizeof(item_t));
     assert(item);
 
@@ -103,22 +103,9 @@ todo_item_new(void) {
     return item;
 }
 
-item_t *
-todo_item_build(const char *name, const char *description, const char *notes, unsigned long due, unsigned long created) {
-    item_t *item = todo_item_new();
-
-    todo_set_string(&item->name, name);
-    todo_set_string(&item->description, description);
-    todo_set_string(&item->notes, notes);
-    item->due = due;
-    item->created = created;
-
-    return item;
-}
-
 int
-todo_item_save(item_t *item) {
-    static char item_save_format[] = "INSERT INTO todo_item(name, description, notes, due, created, groupid) VALUES('%s', '%s', '%s', %ld, %ld, %d);";
+item_save(item_t *item) {
+    static char item_save_format[] = "INSERT INTO item(name, description, notes, due, created, groupid) VALUES('%s', '%s', '%s', %ld, %ld, %d);";
     char *sql = NULL;
     char *err_msg;
     int rc;
@@ -127,23 +114,23 @@ todo_item_save(item_t *item) {
 
     assert(sql);
 
-    rc = sqlite3_exec(todo_db, sql, 0, 0, &err_msg);
+    rc = sqlite3_exec(db, sql, 0, 0, &err_msg);
     if (rc != SQLITE_OK) {
         fprintf(stderr, "Failed to save item: %s\n", err_msg);
         sqlite3_free(err_msg);
     }
 
-    item->id = sqlite3_last_insert_rowid(todo_db);
+    item->id = sqlite3_last_insert_rowid(db);
 
     free(sql);
 
     return rc;
 }
 
-int _todo_item_load(void *udp, int c_num, char **c_vals, char **c_names) {
+int _item_load(void *udp, int c_num, char **c_vals, char **c_names) {
     item_list_t *list = (item_list_t *)udp;
 
-    item_t *item = todo_item_new();
+    item_t *item = item_new();
 
     for (int i = 0; i < c_num; i++) {
         if (strcmp(c_names[i], "id") == 0) {
@@ -151,15 +138,15 @@ int _todo_item_load(void *udp, int c_num, char **c_vals, char **c_names) {
         }
 
         if (strcmp(c_names[i], "name") == 0) {
-            todo_set_string(&item->name, c_vals[i]);
+            set_string(&item->name, c_vals[i]);
         }
 
         if (strcmp(c_names[i], "description") == 0) {
-            todo_set_string(&item->description, c_vals[i]);
+            set_string(&item->description, c_vals[i]);
         }
 
         if (strcmp(c_names[i], "notes") == 0) {
-            todo_set_string(&item->notes, c_vals[i]);
+            set_string(&item->notes, c_vals[i]);
         }
 
         if (strcmp(c_names[i], "due") == 0) {
@@ -179,15 +166,15 @@ int _todo_item_load(void *udp, int c_num, char **c_vals, char **c_names) {
 }
 
 item_list_t *
-todo_item_load(void) {
-    static char sql[] = "SELECT * FROM todo_item;";
+item_load(void) {
+    static char sql[] = "SELECT * FROM item;";
     int rc;
     char *err_msg;
     item_list_t *list = item_list_init();
 
-    rc = sqlite3_exec(todo_db, sql, _todo_item_load, list, &err_msg);
+    rc = sqlite3_exec(db, sql, _item_load, list, &err_msg);
     if (rc != SQLITE_OK) {
-        fprintf(stderr, "Failed to query todo_item %s\n", err_msg);
+        fprintf(stderr, "Failed to query item %s\n", err_msg);
         sqlite3_free(err_msg);
     }
 
@@ -195,20 +182,23 @@ todo_item_load(void) {
 }
 
 item_t *
-todo_item_load_one(unsigned int id) {
+item_load_one(unsigned int id) {
     char *sql;
     int rc;
     char *err_msg;
     item_list_t *list = item_list_init();
     item_t *item = NULL;
 
-    asprintf(&sql, "SELECT * FROM todo_item WHERE id = %d", id);
+    asprintf(&sql, "SELECT * FROM item WHERE id = %d", id);
     assert(sql);
 
-    rc = sqlite3_exec(todo_db, sql, _todo_item_load, list, &err_msg);
+    rc = sqlite3_exec(db, sql, _item_load, list, &err_msg);
     if (rc != SQLITE_OK) {
-      fprintf(stderr, "Failed to query todo_item %s\n", err_msg);
+      fprintf(stderr, "Failed to query item %s\n", err_msg);
       sqlite3_free(err_msg);
+      item_list_free(list);
+      free(sql);
+      return NULL;
     }
 
     free(sql);
@@ -221,9 +211,9 @@ todo_item_load_one(unsigned int id) {
 }
 
 void
-todo_item_free(item_t *item) {
-    todo_free(item->name);
-    todo_free(item->description);
-    todo_free(item->notes);
-    todo_free(item);
+item_free(item_t *item) {
+    free(item->name);
+    free(item->description);
+    free(item->notes);
+    free(item);
 }
--- lib/item.h
+++ lib/item.h
@@ -17,28 +17,106 @@ typedef struct {
   int capacity;
 } item_list_t;
 
-item_list_t *item_list_init(void);
+/**
+ * Function: item_list_new
+ * -----------------------
+ * Creates a new empty item list struct
+ *
+ * \returns newly created item list
+ */
+item_list_t *item_list_new(void);
 
+/**
+ * Function: item_list_add
+ * -----------------------
+ * Adds item to item_list string
+ *
+ * \param list: pointer to list to struct to add to
+ * \param item: pointer to item to add to list
+ */
 void item_list_add(item_list_t *list, item_t *item);
 
+/**
+ * Function: item_list_delete
+ * --------------------------
+ * Delete item from item list by array index
+ *
+ * \param list: pointer to item list
+ * \param index: index of item to remove
+ */
 void item_list_delete(item_list_t *list, int index);
 
+/**
+ * Function: item_list_delete_id
+ * -----------------------------
+ * Delete item from item list by id
+ *
+ * \param list: pointer to item list
+ * \param id: id of item to remove
+ */
 void item_list_delete_id(item_list_t *list, int id);
 
+/**
+ * Function: item_list_free_all
+ * ----------------------------
+ * Free all items from item list and list itself
+ *
+ * \param list: pointer to item list
+ */
 void item_list_free_all(item_list_t *list);
 
+/**
+ * Function: item_list_free_all
+ * ----------------------------
+ * Free item list struct
+ *
+ * \param list: pointer to item list
+ */
 void item_list_free(item_list_t *list);
 
-item_t *todo_item_new(void);
+/**
+ * Function: item_new
+ * ------------------
+ * Create new empty item struct
+ *
+ * \returns newly created empty item struct
+ */
+item_t *item_new(void);
 
-item_t *todo_item_build(const char *name, const char *description, const char *notes, unsigned long due, unsigned long created);
+/**
+ * Function: item_load_one
+ * -----------------------
+ * Load single item from database by id
+ *
+ * \param id: id of item to load
+ *
+ * \returns populated item struct
+ */
+item_t *item_load_one(unsigned int id);
 
-item_t *todo_item_load_one(unsigned int id);
+/**
+ * Function: item_load
+ * -------------------
+ * Load all items from database
+ *
+ * \returns list of populated items
+ */
+item_list_t *item_load(void);
 
-item_list_t *todo_item_load(void);
+/**
+ * Function: item_save
+ * -------------------
+ * Save item into database
+ *
+ * \param item: item to save
+ *
+ * \returns: 0 on success, non zero on fail
+ */
+int item_save(item_t *item);
 
-int todo_item_save(item_t *item);
-
-void todo_item_free(item_t *item);
+/**
+ * F
+ */
+void item_free(item_t *item);
 
 #endif /* __LIB_TODO_ITEM_H */
